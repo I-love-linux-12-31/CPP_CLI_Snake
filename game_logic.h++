e8e0d257b5270e286724271696dc00a4c808d3be
yaroslav_admin <I-love-linux-12-31@users.noreply.github.com>
2022-09-21 14:58:48 +0300

    -

--- game_logic.h++
+++ game_logic.h++
@@ -4,12 +4,25 @@
 #include "posix_only.h++"
 
 #include <string>
+#include <vector>
 
 static const std::string APPLE = "*";
 
 bool inited = false;
 
 long int score = 0;
+
+std::vector<std::vector<int>> snake;
+int_fast8_t vector_y = 0;
+int_fast8_t vector_x = 1;
+
+
+void create_snake () {
+    std::vector<int> pos = {get_terminal_height() / 2, get_terminal_width() / 2};
+    snake.push_back(pos);
+    pos = {get_terminal_height() / 2, get_terminal_width() / 2 - 1};
+    snake.push_back(pos);
+}
 
 
 #endif //GAME_LOGIC_H
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "game_logic.h++"
 
 #include <iostream>
+#include <vector>
 
 int main ()
 {
@@ -9,15 +10,16 @@ int main ()
 
     start_input_processing_thread();
 
+
     std::string map[get_terminal_height() - 1][get_terminal_width()];
     for (unsigned int y = 0; y < get_terminal_height() - 1; y++){
         for (unsigned int x = 0; x < get_terminal_width(); x++){
-            map[y][x] = "#";
+            map[y][x] = " ";
         }
     }
-    for (unsigned int i = 0; i < get_terminal_width(); i++){
-        map[2][i] = APPLE;
-    }
+//    for (unsigned int i = 0; i < get_terminal_width(); i++){
+//        map[2][i] = APPLE;
+//    }
 
     while (can_run){
 
@@ -27,8 +29,24 @@ int main ()
             }
             std::cout << std::endl;
         }
+
+        for (unsigned int i = 0; i < snake.size(); i++){
+            std::cout << "\033[" << snake[i][1] << ";" << snake[i][0] <<"H";
+            if (i == 0){
+                std::cout << "$";
+            }
+            else{
+                std::cout << "#";
+            }
+
+        }
+
+
+
+
         std::cout << "\033[0;0H";
         sleep(1);
+
     }
 
 
